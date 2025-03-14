8e65fbb27b84bb968fe19c78e644849e30a12272
mainlego <mainvm@outlook.com>
2025-03-14 15:18:48 +0200

    first commit

--- render-start.js
+++ render-start.js
@@ -1,6 +1,24 @@
 // render-start.js
 import { createRequire } from 'module';
+import dbConnect from './lib/dbConnect.js';
 const require = createRequire(import.meta.url);
+
+
+(async () => {
+    try {
+        await dbConnect(); // Подключаемся к базе данных
+        console.log("База данных успешно подключена");
+
+        // Запускаем бота
+        import('./bot.js').catch(err => {
+            console.error('Ошибка запуска бота:', err);
+            process.exit(1);
+        });
+    } catch (error) {
+        console.error('Ошибка подключения к базе данных:', error);
+        process.exit(1);
+    }
+})();
 
 // Загружаем переменные окружения из process.env
 process.env.TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
